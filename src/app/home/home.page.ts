312deb98aa1c94462ed2cc28b9c016717fec979f
santDiaz <santi.diazok2001@gmail.com>
2022-06-25 17:43:47 +0200

    Casi casi

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLinkWithHref } from '@angular/router';
 import { InteractionService } from '../services/interaction.service';
 import { FirestoreService } from '../services/firestore.service';
 import { SlideI } from '../models/models';
@@ -17,43 +17,7 @@ export class HomePage {
 
   slides: SlideI[] =[];
    nuevoSlide: SlideI;
-  // slides  = [
-  //   {
-  //     img:  '../../assets/icons/poblacion.png',
-  //     titulo: 'Poblacion',
-  //     valor: '46.234.830',
-  //     tiempo: 'Estimacion 2022',
-  //     url: 'https://www.indec.gob.ar/uploads/informesdeprensa/mercado_trabajo_eph_4trim211A57838DEC.pdf',
-  //   },
-  //   {
-  //     img:  '../../assets/icons/desempleo2.png',
-  //     titulo: 'Tasa de desocupacion',
-  //     valor: '7,0%',
-  //     tiempo: 'Cuatro trimestre 2021',
-  //     url: 'https://www.indec.gob.ar/uploads/informesdeprensa/mercado_trabajo_eph_4trim211A57838DEC.pdf',
-  //   },
-  //   {
-  //     img:  '../../assets/icons/desempleo.png ',
-  //     titulo: 'Precio al consumidor',
-  //     valor: '5,1%',
-  //     tiempo: 'Mayo 2022',
-  //     url: 'https://www.indec.gob.ar/uploads/informesdeprensa/mercado_trabajo_eph_4trim211A57838DEC.pdf',
-  //   },
-  //   {
-  //     img:  '../../assets/icons/manufacturero.png ',
-  //     titulo: 'Indice de produccion industrial manufacturero',
-  //     valor: '5,0%',
-  //     tiempo: 'Abril 2022',
-  //     url: 'https://www.indec.gob.ar/uploads/informesdeprensa/mercado_trabajo_eph_4trim211A57838DEC.pdf',
-  //   },
-  //   {
-  //     img:  '../../assets/icons/economia.png ',
-  //     titulo: 'Estimador mensual de actividad economica',
-  //     valor: '-0,7%',
-  //     tiempo: 'Marzo 2022',
-  //     url: 'https://www.indec.gob.ar/uploads/informesdeprensa/mercado_trabajo_eph_4trim211A57838DEC.pdf',
-  //   },
-  // ]
+ 
 
   constructor(private menuCtrl: MenuController,
               private auth: AuthService,
@@ -97,9 +61,12 @@ logout(){
 }
 
 redirecTo(){
-  this.router.navigate(['slides.url'])
+console.log("si");
+
 }
 
+marcar(){
 
+}
 
 }
